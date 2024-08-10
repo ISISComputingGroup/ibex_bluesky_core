8111e96c14df504dcde4e88ccc46afa41162a3a4
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2024-08-10 16:26:18 +0100

    Add basic block signals

--- tests/devices/test_block.py
+++ tests/devices/test_block.py
@@ -1,11 +1,11 @@
 import pytest
-from ibex_bluesky_core.devices.block import Block
+from ibex_bluesky_core.devices.block import BlockRwRbv
 from ophyd_async.core import get_mock_put, set_mock_value
 
 
 @pytest.fixture
-async def float_block() -> Block[float]:
-    block = Block("UNITTEST:MOCK:", "float_block", float)
+async def float_block() -> BlockRwRbv[float]:
+    block = BlockRwRbv(float, "UNITTEST:MOCK:", "float_block")
     await block.connect(mock=True)
     return block
 
@@ -24,15 +24,16 @@ def test_block_signal_monitors_correct_pv(float_block):
 async def test_locate(float_block):
     set_mock_value(float_block.readback, 10)
     set_mock_value(float_block.setpoint, 20)
+    set_mock_value(float_block.setpoint_readback, 30)
     location = await float_block.locate()
 
     assert location == {
         "readback": 10,
-        "setpoint": 20,
+        "setpoint": 30,  # Should use SP:RBV not SP
     }
 
 
 async def test_block_set(float_block):
     set_mock_value(float_block.setpoint, 10)
     await float_block.set(20)
-    get_mock_put(float_block.setpoint).assert_called_once_with(20, wait=True, timeout=10)
+    get_mock_put(float_block.setpoint).assert_called_once_with(20, wait=True, timeout=None)
