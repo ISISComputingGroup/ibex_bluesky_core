edad98388134d94c3c723c29cd6d92343ae3cdfe
Jack Harper <jack.harper@stfc.ac.uk>
2024-08-30 13:25:07 +0100

    add beginrunex

--- src/ibex_bluesky_core/devices/dae/dae_controls.py
+++ src/ibex_bluesky_core/devices/dae/dae_controls.py
@@ -1,12 +1,14 @@
-from ophyd_async.core import SignalX, StandardReadable
-from ophyd_async.epics.signal import epics_signal_x
+from enum import IntFlag
+
+from bluesky.protocols import Movable
+from ophyd_async.core import AsyncStatus, SignalRW, SignalX, StandardReadable
+from ophyd_async.epics.signal import epics_signal_rw, epics_signal_x
 
 
 class DaeControls(StandardReadable):
     def __init__(self, dae_prefix, name=""):
         self.begin_run: SignalX = epics_signal_x(f"{dae_prefix}BEGINRUN")
-        self.begin_run_ex: SignalX = epics_signal_x(f"{dae_prefix}BEGINRUNEX")
-
+        self.begin_run_ex: BeginRunEx = BeginRunEx(dae_prefix)
         self.end_run: SignalX = epics_signal_x(f"{dae_prefix}ENDRUN")
         self.pause_run: SignalX = epics_signal_x(f"{dae_prefix}PAUSERUN")
         self.resume_run: SignalX = epics_signal_x(f"{dae_prefix}RESUMERUN")
@@ -15,3 +17,19 @@ class DaeControls(StandardReadable):
         self.save_run: SignalX = epics_signal_x(f"{dae_prefix}SAVERUN")
 
         super().__init__(name=name)
+
+
+class BeginRunExBits(IntFlag):
+    NONE = 0
+    BEGINIFPAUSED = 1
+    BEGINIFDELAYED = 2
+
+
+class BeginRunEx(StandardReadable, Movable):
+    def __init__(self, dae_prefix, name=""):
+        self.begin_run_ex: SignalRW = epics_signal_rw(int, f"{dae_prefix}BEGINRUNEX")
+        super().__init__(name=name)
+
+    @AsyncStatus.wrap
+    async def set(self, value: BeginRunExBits) -> None:
+        await self.begin_run_ex.set(value, wait=True)
