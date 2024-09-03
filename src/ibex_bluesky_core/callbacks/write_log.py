186231dfc38aeab8206cac497e60485b0f62f84e
Daniel Maclaren <daniel.maclaren@stfc.ac.uk>
2024-09-03 17:43:58 +0100

    units and prec added to log file

--- src/ibex_bluesky_core/callbacks/write_log.py
+++ src/ibex_bluesky_core/callbacks/write_log.py
@@ -1,7 +1,10 @@
 """Creates a readable .txt file of Bluesky runengine dataset"""
 
-from pathlib import Path
 import csv
+import decimal
+from datetime import datetime
+from collections import OrderedDict
+from pathlib import Path
 from bluesky.callbacks import CallbackBase
 from event_model.documents.event import Event
 from event_model.documents.event_descriptor import EventDescriptor
@@ -15,7 +18,9 @@ class OutputLoggingCallback(CallbackBase):
 
     def __init__(self) -> None:
         """Initialise current_start_document and filename"""
+        super().__init__()        
         self.current_start_document = None
+        self.descriptors = {}
         self.filename = None
 
     def start(self, doc: RunStart) -> None:
@@ -26,9 +31,12 @@ class OutputLoggingCallback(CallbackBase):
         
         assert self.filename is not None, "Could not create filename."
         assert self.current_start_document is not None, "Saw a non-start document before a start."
-        
-        start_data = ["scan_id", "time"]
 
+        datetime_obj = datetime.fromtimestamp(doc["time"])
+        formatted_time = datetime_obj.strftime('%Y-%m-%d %H:%M:%S')
+        doc.update({"time": formatted_time})
+
+        start_data = ["scan_id", "time"]
         start_data_reordered = {key: doc[key] for key in start_data if key in doc}
 
         for key in doc: 
@@ -40,19 +48,42 @@ class OutputLoggingCallback(CallbackBase):
                 outfile.write('%s: %s\n' % (key,value))
 
     def descriptor(self, doc: EventDescriptor) -> None:
-        return super().descriptor(doc)
-    
-    def event(self, doc: Event) -> Event:
+        if doc['name'] != 'primary':
+            return
+        
+        descriptor_id = doc['uid']
+        self.descriptors[descriptor_id] = doc
+
+    def event(self, doc: Event) -> None:
+        descriptor_id = doc["descriptor"]
         event_data = doc["data"]
-        event_data_reordered = {"seq_num": doc["seq_num"], **event_data}
-        event_data_list = [event_data_reordered]
-        event_data_fieldnames= event_data_reordered.keys()
+        event_data_list = [event_data]
+        event_data_fieldnames = event_data.keys()
+        precision = self.descriptors
+        descriptor_data = precision[descriptor_id]['data_keys']
+        formatted_event_data = {}
+
+        units_dict = OrderedDict({key: value.get('units', 'n/a') for key, value in descriptor_data.items()})
+        units_line = '   '.join(f'{key} ({value})' for key, value in units_dict.items())
+        precision_dict = {key: value.get('precision', 'n/a') for key, value in descriptor_data.items()}
+        precision_line = '   '.join(f'{key} ({value})' for key, value in precision_dict.items())
+
+        for key, value in event_data.items():
+            if key in precision_dict and isinstance(value, float):
+                if isinstance(precision_dict[key], int):
+                    formatted_value = f"{value:.{precision_dict[key]}f}"
+                    formatted_event_data[key] = formatted_value
+                else: 
+                    formatted_event_data[key] = value
+
+        formatted_event_data_list = [formatted_event_data]
 
         with open(self.filename, "a", newline='') as outfile:
-            writer = csv.DictWriter(outfile, fieldnames=event_data_fieldnames, delimiter='\t')
             if doc["seq_num"] == 1:
-                writer.writeheader()
-            writer.writerows(event_data_list)
-    
+                outfile.write(f"{units_line}\n")
+            writer = csv.DictWriter(outfile, fieldnames=event_data_fieldnames, delimiter='\t')
+            writer.writerows(formatted_event_data_list)
+        
     def stop(self, doc: RunStop) -> None:
+        self.descriptors.clear()
         return super().stop(doc)
