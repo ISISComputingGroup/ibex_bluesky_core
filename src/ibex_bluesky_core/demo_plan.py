fb8cb84472438fef02305c237c5cf019eb7225e0
Jack Harper <jack.harper@stfc.ac.uk>
2024-09-03 13:57:20 +0100

    add comment for demo plan

--- src/ibex_bluesky_core/demo_plan.py
+++ src/ibex_bluesky_core/demo_plan.py
@@ -3,42 +3,43 @@
 from typing import Generator
 
 import bluesky.plan_stubs as bps
+import matplotlib
+import matplotlib.pyplot as plt
 from bluesky.callbacks import LiveTable
-from bluesky.preprocessors import run_decorator
+from bluesky.preprocessors import run_decorator, subs_decorator
 from bluesky.utils import Msg
 from ophyd_async.plan_stubs import ensure_connected
 
+from ibex_bluesky_core.callbacks.plotting import LivePlot
 from ibex_bluesky_core.devices import get_pv_prefix
-from ibex_bluesky_core.devices.block import BlockRwRbv, block_rw_rbv
-from ibex_bluesky_core.devices.dae.dae import Dae
+from ibex_bluesky_core.devices.block import block_rw_rbv
+from ibex_bluesky_core.devices.dae import Dae
 from ibex_bluesky_core.run_engine import get_run_engine
 
-__all__ = ["run_demo_plan", "demo_plan"]
+__all__ = ["demo_plan"]
 
 
-def run_demo_plan() -> None:
-    """Run the demo plan, including setup which would usually be done outside the plan.
-
-    You will need a DAE in a state which can begin, and a settable & readable
-    floating-point block named "mot".
-
-    Run using:
-    >>> from ibex_bluesky_core.demo_plan import run_demo_plan
-    >>> run_demo_plan()
-    """
-    RE = get_run_engine()
+def demo_plan() -> Generator[Msg, None, None]:
+    """Demonstration plan which moves a block and reads the DAE."""
     prefix = get_pv_prefix()
     block = block_rw_rbv(float, "mot")
     dae = Dae(prefix)
-    RE(demo_plan(block, dae), LiveTable(["mot", "DAE"]))
 
-
-def demo_plan(block: BlockRwRbv[float], dae: Dae) -> Generator[Msg, None, None]:
-    """Demonstration plan which moves a block and reads the DAE."""
     yield from ensure_connected(block, dae, force_reconnect=True)
 
+    @subs_decorator(
+        [
+            LivePlot(y=dae.name, x=block.name, marker="x", linestyle="none"),
+            LiveTable([block.name, dae.name]),
+        ]
+    )
     @run_decorator(md={})
     def _inner() -> Generator[Msg, None, None]:
+        # A "simple" acquisition using trigger_and_read.
+        # Currently this does not work as the DAE does not have a trigger method.
+        # yield from bps.abs_set(block, 1.0, wait=True)
+        # yield from bps.trigger_and_read([block, dae])
+
         # More complicated acquisition showing arbitrary DAE control to support complex use-cases.
         yield from bps.abs_set(block, 2.0, wait=True)
         yield from bps.trigger(dae.controls.begin_run, wait=True)
@@ -50,3 +51,12 @@ def demo_plan(block: BlockRwRbv[float], dae: Dae) -> Generator[Msg, None, None]:
         yield from bps.save()
 
     yield from _inner()
+
+
+if __name__ == "__main__":
+    if "genie_python" not in matplotlib.get_backend():
+        matplotlib.use("qtagg")
+        plt.ion()
+    RE = get_run_engine()
+    RE(demo_plan())
+    input("plan complete, press return to continue.")
