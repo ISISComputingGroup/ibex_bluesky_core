49d2c777d2630f14cc94893aad0b5c7be52a16c7
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-08 12:12:25 +0100

    add comment about preprocessors in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,6 +8,8 @@ from ibex_bluesky_core.run_engine import get_run_engine
 def RE() -> RunEngine:
     get_run_engine.cache_clear()
     re =  get_run_engine()
+    # Clear the preprocessors as the file writing one relies
+    # on a real signal which won't exist during testing
     re.preprocessors.clear()
 
     return re
