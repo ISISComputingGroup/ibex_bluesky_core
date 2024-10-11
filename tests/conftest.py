fcd139bd52d2a80f40f19eddf5ff2094b00d3630
Jack Harper <jack.harper@stfc.ac.uk>
2024-10-11 09:13:56 +0100

    preprocessor clearing docstring fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,7 +10,7 @@ MOCK_PREFIX = "UNITTEST:MOCK:"
 def RE() -> RunEngine:
     get_run_engine.cache_clear()
     re = get_run_engine()
-    # Clear the preprocessors as the file writing one relies
+    # Clear the preprocessors as the rb number metadata injector relies
     # on a real signal which won't exist during testing
     re.preprocessors.clear()
 
