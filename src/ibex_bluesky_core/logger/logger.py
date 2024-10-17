cd33ec515c2dab175cdfef91f6586bae7aea7fa6
Chsudeepta <Chsudeepta@NDW2633.isis.cclrc.ac.uk>
2024-10-17 12:48:22 +0100

    Ruff reformatting done

--- src/ibex_bluesky_core/logger/logger.py
+++ src/ibex_bluesky_core/logger/logger.py
@@ -1,4 +1,5 @@
 """Bluesky specific logging utility."""
+
 import logging
 import logging.config
 import os
@@ -10,17 +11,17 @@ To use me
 2. Use me at the level you want --> logger.blueskylogger.info("Some useful message")
 3. To change the log level check the logging.conf file"""
 
-BLUESKY_LOGGER = 'bluesky'
-DEFAULT_LOGGER = 'blueskylogs.log'
+BLUESKY_LOGGER = "bluesky"
+DEFAULT_LOGGER = "blueskylogs.log"
 LOG_FOLDER = os.path.join("C:\\", "instrument", "var", "logs", BLUESKY_LOGGER)
 
 # Find the log directory, if already set in the environment, else use the default
 log_location = os.environ.get("BLUESKY_LOGS", LOG_FOLDER)
 # Create the log directory if it doesn't already exist
-os.makedirs(log_location, exist_ok = True)
+os.makedirs(log_location, exist_ok=True)
 
 filepath = Path(__file__).resolve().parent
-#disable_existing_loggers ensures all loggers have same configuration as below
-logging.config.fileConfig(os.path.join(filepath, 'logging.conf'), disable_existing_loggers=False)
+# disable_existing_loggers ensures all loggers have same configuration as below
+logging.config.fileConfig(os.path.join(filepath, "logging.conf"), disable_existing_loggers=False)
 
-blueskylogger = logging.getLogger('blueskycore')
+blueskylogger = logging.getLogger("blueskycore")
--- tests/logger/test_logger.py
+++ tests/logger/test_logger.py
@@ -1,4 +1,3 @@
-
 import logging
 import sys
 
@@ -7,8 +6,9 @@ from ibex_bluesky_core.logger import logger
 LOG_MESSAGE = "Logging something to "
 LOG_FILE_NAME = "blueskylogs.log"
 
+
 def test_GIVEN_logging_is_requested_THEN_handler_is_added():
-    this_function_name = sys._getframe(  ).f_code.co_name
+    this_function_name = sys._getframe().f_code.co_name
     message = LOG_MESSAGE + this_function_name
     # Log invocation.
     logger.blueskylogger.info(message)
@@ -19,6 +19,6 @@ def test_GIVEN_logging_is_requested_THEN_handler_is_added():
             loghandler = handler
 
     assert loghandler is not None
-    assert loghandler.name == 'timedRotatingFileHandler'
-    assert loghandler.when.lower() == 'midnight'
+    assert loghandler.name == "timedRotatingFileHandler"
+    assert loghandler.when.lower() == "midnight"
     assert loghandler.baseFilename.endswith(LOG_FILE_NAME)
