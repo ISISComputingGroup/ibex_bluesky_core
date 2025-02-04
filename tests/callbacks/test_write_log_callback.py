eb325e16da9b7f2d8dff2107b8fa4f6d47066ab7
Jack Harper <jack.harper@stfc.ac.uk>
2025-02-04 11:49:12 +0000

    fix and add test

--- tests/callbacks/test_write_log_callback.py
+++ tests/callbacks/test_write_log_callback.py
@@ -22,7 +22,9 @@ def test_header_data_all_available_on_start(cb):
     time = 1728049423.5860472
     uid = "test123"
     scan_id = 1234
-    run_start = RunStart(time=time, uid=uid, scan_id=scan_id, rb_number="0")
+    run_start = RunStart(
+        time=time, uid=uid, scan_id=scan_id, rb_number="0", detectors=["dae"], motors=("block",)
+    )
     with (
         patch("ibex_bluesky_core.callbacks.file_logger.open", mock_open()) as mock_file,
         patch("ibex_bluesky_core.callbacks.file_logger.os.makedirs"),
@@ -31,7 +33,7 @@ def test_header_data_all_available_on_start(cb):
         result = (
             save_path
             / f"{run_start.get('rb_number', None)}"
-            / f"{node()}_block_dae_2024-10-04_14-43-43Z.txt"
+            / f"{node()}_dae_block_2024-10-04_14-43-43Z.txt"
         )
 
     mock_file.assert_called_with(result, "a", newline="\n", encoding="utf-8")
@@ -45,14 +47,35 @@ def test_no_rb_number_folder(cb):
     time = 1728049423.5860472
     uid = "test123"
     scan_id = 1234
-    run_start = RunStart(time=time, uid=uid, scan_id=scan_id)
+    run_start = RunStart(time=time, uid=uid, scan_id=scan_id, detectors=["dae"], motors=("block",))
 
     with (
         patch("ibex_bluesky_core.callbacks.file_logger.open", mock_open()) as mock_file,
         patch("ibex_bluesky_core.callbacks.file_logger.os.makedirs") as mock_mkdir,
     ):
         cb.start(run_start)
-        result = save_path / "Unknown RB" / f"{node()}_block_dae_2024-10-04_14-43-43Z.txt"
+        result = save_path / "Unknown RB" / f"{node()}_dae_block_2024-10-04_14-43-43Z.txt"
+        assert mock_mkdir.called
+
+    mock_file.assert_called_with(result, "a", newline="\n", encoding="utf-8")
+    # time should have been renamed to start_time and converted to human readable
+    writelines_call_args = mock_file().writelines.call_args[0][0]
+    assert "start_time: 2024-10-04 14:43:43\n" in writelines_call_args
+    assert f"uid: {uid}\n" in writelines_call_args
+
+
+def test_no_motors_doesnt_append_to_filename(cb):
+    time = 1728049423.5860472
+    uid = "test123"
+    scan_id = 1234
+    run_start = RunStart(time=time, uid=uid, scan_id=scan_id, detectors=["dae"])
+
+    with (
+        patch("ibex_bluesky_core.callbacks.file_logger.open", mock_open()) as mock_file,
+        patch("ibex_bluesky_core.callbacks.file_logger.os.makedirs") as mock_mkdir,
+    ):
+        cb.start(run_start)
+        result = save_path / "Unknown RB" / f"{node()}_dae_2024-10-04_14-43-43Z.txt"
         assert mock_mkdir.called
 
     mock_file.assert_called_with(result, "a", newline="\n", encoding="utf-8")
