b1b2e7e39e120edf5ef14b26920a280b7b24c72c
esmithExperimentControls <evan.smith@stfc.ac.uk>
2025-02-05 14:14:27 +0000

    Add more unit tests

--- tests/devices/simpledae/test_reducers.py
+++ tests/devices/simpledae/test_reducers.py
@@ -1,3 +1,4 @@
+# pyright: reportMissingParameterType=false
 import math
 from unittest.mock import AsyncMock
 
@@ -11,9 +12,9 @@ from ibex_bluesky_core.devices.simpledae.reducers import (
     MonitorNormalizer,
     PeriodGoodFramesNormalizer,
     ScalarNormalizer,
+    polarization,
     tof_bounded_spectra,
     wavelength_bounded_spectra,
-    polarization
 )
 
 
@@ -936,25 +937,82 @@ def test_wavelength_bounded_spectra_bounds_missing_or_too_many(data: list[float]
             total_flight_path_length=sc.scalar(value=100, unit=sc.units.m),
         )
 
+
 # Polarization
-@pytest.mark.parametrize("A, B, sigma_A, sigma_B, expected_value, expected_uncertainty", [
+@pytest.mark.parametrize(
+    ("a", "b", "variance_a", "variance_b", "expected_value", "expected_uncertainty"),
+    [
         # Case 1: Symmetric case with equal uncertainties
-        (5.0, 3.0, 0.1, 0.1, 0.25, 0.0354),
-        
+        (5.0, 3.0, 0.1, 0.1, 0.25, 0.05762215285808055),  # comment where tf this number came from
         # Case 2: Asymmetric case with different uncertainties
-        (10.0, 6.0, 0.2, 0.3, 0.25, 0.0884),
-        
+        (10.0, 6.0, 0.2, 0.3, 0.25, 0.04764984588117782),
         # Case 3: Case with larger values and different uncertainty magnitudes
-        (100.0, 60.0, 1.0, 2.0, 0.25, 0.0884)
-    ])
-def test_polarization_function_calculates_accurately(A: float, B: float, sigma_A: float, sigma_B: float,
-                                                    expected_value: float, expected_uncertainty: float):
-    var_a = sc.Variable(dims=[], values=A, variances=sigma_A, unit='')
-    var_b = sc.Variable(dims=[], values=B, variances=sigma_B, unit='')
+        (100.0, 60.0, 1.0, 2.0, 0.25, 0.0120017902310447),
+    ],
+)
+def test_polarization_function_calculates_accurately(
+    a, b, variance_a, variance_b, expected_value, expected_uncertainty
+):
+    var_a = sc.Variable(dims=[], values=a, variances=variance_a, unit="", dtype="float64")
+    var_b = sc.Variable(dims=[], values=b, variances=variance_b, unit="", dtype="float64")
     result_value = polarization(var_a, var_b)
 
-    result_uncertainy = result_value.variance
+    result_uncertainy = (result_value.variance) ** 0.5  # uncertainty is sqrt of variance
 
-    assert result_value == expected_value
-    assert result_uncertainy == expected_uncertainty
+    assert result_value == pytest.approx(expected_value)
+    assert result_uncertainy == pytest.approx(expected_uncertainty)
 
+
+# test that arrays are supported
+@pytest.mark.parametrize(
+    ("a", "b", "variances_a", "variances_b", "expected_values", "expected_uncertainties"),
+    [
+        # Case 1: Symmetric case with equal uncertainties
+        (
+            [5.0, 10.0, 100.0],
+            [3.0, 6.0, 60.0],
+            [0.1, 0.2, 1.0],
+            [0.1, 0.3, 2.0],
+            [0.25, 0.25, 0.25],
+            [0.05762215285808055, 0.04764984588117782, 0.0120017902310447],
+        ),
+        # These uncertainty numbers were calculated using python's 'uncertainties' library
+    ],
+)
+def test_polarization_2_arrays(
+    a, b, variances_a, variances_b, expected_values, expected_uncertainties
+):
+    var_a = sc.Variable(dims=["x"], values=a, variances=variances_a, unit="")
+    var_b = sc.Variable(dims=["x"], values=b, variances=variances_b, unit="")
+
+    result_value = polarization(var_a, var_b)
+
+    result_uncertainties = (result_value.variances) ** 0.5
+
+    assert result_value == pytest.approx(expected_values)
+    assert result_uncertainties == pytest.approx(expected_uncertainties)
+
+
+# test that units don't match
+def test_polarization_units_mismatch():
+    var_a = sc.Variable(dims=["x"], values=[1], variances=[0.1], unit="m", dtype="float64")
+    var_b = sc.Variable(dims=["x"], values=[1], variances=[0.1], unit="u", dtype="float64")
+
+    with pytest.raises(
+        expected_exception=ValueError, match="The units of a and b are not equivalent."
+    ):
+        polarization(var_a, var_b)
+
+
+# test that arrays are of unmatching sizes
+def test_polarization_arrays_of_different_sizes():
+    var_a = sc.Variable(dims=["x"], values=[1, 2], variances=[0.1, 0.1], unit="m", dtype="float64")
+    var_b = sc.Variable(dims=["x"], values=[1], variances=[0.1], unit="m", dtype="float64")
+
+    with pytest.raises(
+        expected_exception=ValueError, match="Dimensions/shape of a and b must match."
+    ):
+        polarization(var_a, var_b)
+
+
+# test cases uncert = 0
