db966d163da2684c9b6efedfc31079ee3493a2fe
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-03-06 16:42:21 +0000

    Add unit test

--- tests/plans/test_det_map_align.py
+++ tests/plans/test_det_map_align.py
@@ -2,17 +2,22 @@
 from unittest.mock import patch
 
 import numpy as np
+import pytest
+from ophyd_async.core import soft_signal_rw
+from ophyd_async.testing import callback_on_mock_put, set_mock_value
 
-from ibex_bluesky_core.devices.block import BlockRw
-from ibex_bluesky_core.devices.simpledae import Controller, SimpleDae, Waiter
+from ibex_bluesky_core.devices.simpledae import SimpleDae
 from ibex_bluesky_core.devices.simpledae.reducers import PeriodSpecIntegralsReducer
+from ibex_bluesky_core.devices.simpledae.strategies import Controller, Waiter
 from ibex_bluesky_core.plans.reflectometry.det_map_align import mapping_alignment_plan
 
 
 async def test_det_map_align(RE):
     noop_controller = Controller()
     noop_waiter = Waiter()
-    reducer = PeriodSpecIntegralsReducer(monitors=np.array([1]), detectors=np.array([2, 3, 4]))
+    reducer = PeriodSpecIntegralsReducer(
+        monitors=np.array([1]), detectors=np.array([2, 3, 4, 5, 6])
+    )
 
     dae = SimpleDae(
         prefix="unittest:",
@@ -21,21 +26,48 @@ async def test_det_map_align(RE):
         waiter=noop_waiter,
         reducer=reducer,
     )
-    height = BlockRw(prefix="unittest:", block_name="height", datatype=float)
+    height = soft_signal_rw(float, name="height")
 
     await dae.connect(mock=True)
     await height.connect(mock=True)
+
+    set_mock_value(dae.number_of_periods, 6)
+    set_mock_value(dae.num_spectra, 6)
+    set_mock_value(dae.num_time_channels, 1)
+
+    specdata = np.array([
+        0, 0, 0, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 5000, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0,
+        0, 0, 0, 5000, 0, 1, 0, 2, 0, 3, 0, 2, 0, 1,
+        0, 0, 0, 5000, 0, 1, 0, 2, 0, 3, 0, 2, 0, 1,
+        0, 0, 0, 5000, 0, 0, 0, 1, 0, 2, 0, 1, 0, 0,
+        0, 0, 0, 5000, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+    ])  # fmt: skip
+
+    set_mock_value(dae.raw_spec_data, specdata)
+    set_mock_value(dae.raw_spec_data_nord, len(specdata))
+
+    period_num = 0
+
+    def _increment_period_num(*_, **__):
+        nonlocal period_num
+        period_num += 1
+        set_mock_value(dae.period_num, period_num)
+
+    callback_on_mock_put(height, _increment_period_num)
 
     with patch("ibex_bluesky_core.plans.reflectometry.det_map_align.ensure_connected"):
         result = RE(
             mapping_alignment_plan(
                 dae=dae,  # type: ignore
-                height=height,
+                height=height,  # type: ignore
                 start=0,
                 stop=10,
-                num=10,
-                angle_map=np.array([1, 2, 3]),
+                num=6,
+                angle_map=np.array([21, 22, 23, 24, 25]),
             )
         )
 
-    assert result == {}
+    print(result.plan_result)
+    assert result.plan_result["height_fit"].params["x0"].value == pytest.approx(5.0)
+    assert result.plan_result["angle_fit"].params["x0"].value == pytest.approx(23.0)
