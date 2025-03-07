a33e61423eba9fffe08f716c3fe80cff580a10bb
Jack Harper <jack.harper@stfc.ac.uk>
2025-03-07 14:48:25 +0000

    whoops - use args for detectors and single block

--- tests/plans/test_init.py
+++ tests/plans/test_init.py
@@ -2,9 +2,13 @@
 
 from unittest.mock import patch
 
+import pytest
+
 from ibex_bluesky_core.devices.block import BlockMot
 from ibex_bluesky_core.devices.simpledae import SimpleDae
-from ibex_bluesky_core.plans import motor_adaptive_scan, motor_scan
+from ibex_bluesky_core.devices.simpledae.reducers import MonitorNormalizer
+from ibex_bluesky_core.devices.simpledae.strategies import Controller, Waiter
+from ibex_bluesky_core.plans import adaptive_scan, motor_adaptive_scan, motor_scan, scan
 
 
 def test_scan_motor_creates_block_device_and_dae(RE):
@@ -47,12 +51,113 @@ def test_adaptive_scan_motor_creates_block_device_and_dae(RE):
         assert scan.call_args[1]["block"].name == block_name
 
 
-# test scan does normal scan with mock
-# test scan does relative scan with mock
+@pytest.fixture
+async def dae():
+    noop_controller = Controller()
+    noop_waiter = Waiter()
+    reducer = MonitorNormalizer(prefix="UNITTEST:", detector_spectra=[1, 2, 3], monitor_spectra=[4])
 
-# test adaptive_scan does normal adaptive scan with mock
-# test adaptive_scan does relative adaptive scan with mock
+    dae = SimpleDae(
+        prefix="UNITTEST:",
+        name="dae",
+        controller=noop_controller,
+        waiter=noop_waiter,
+        reducer=reducer,
+    )
+    await dae.connect(mock=True)
+    return dae
+
+
+@pytest.fixture
+async def block():
+    block = BlockMot(prefix="UNITTEST:", block_name="SOME_BLOCK")
+    await block.connect(mock=True)
+    return block
+
+
+def test_scan_does_normal_scan_when_relative_false(RE, dae, block):
+    start = 0
+    stop = 2
+    count = 3
+    with (
+        patch("ibex_bluesky_core.plans.bp.scan") as bp_scan,
+        patch("ibex_bluesky_core.plans.ensure_connected"),
+    ):
+        RE(scan(dae, block, start, stop, count, rel=False))
+    bp_scan.assert_called_once()
+    assert dae in bp_scan.call_args[0][0]
+    assert block == bp_scan.call_args[0][1]
+    assert start == bp_scan.call_args[0][2]
+    assert stop == bp_scan.call_args[0][3]
+    assert count == bp_scan.call_args[1]["num"]
+
+
+def test_scan_does_relative_scan_when_relative_true(RE, dae, block):
+    start = 0
+    stop = 2
+    count = 3
+    with (
+        patch("ibex_bluesky_core.plans.bp.rel_scan") as bp_scan,
+        patch("ibex_bluesky_core.plans.ensure_connected"),
+    ):
+        RE(scan(dae, block, start, stop, count, rel=True))
+    bp_scan.assert_called_once()
+    assert dae in bp_scan.call_args[0][0]
+    assert block == bp_scan.call_args[0][1]
+    assert start == bp_scan.call_args[0][2]
+    assert stop == bp_scan.call_args[0][3]
+    assert count == bp_scan.call_args[1]["num"]
+
+
+def test_adaptive_scan_does_normal_scan_when_relative_false(RE, dae, block):
+    start = 0
+    stop = 2
+    min_step = 0.01
+    max_step = 0.1
+    target_delta = 0.5
+    with (
+        patch("ibex_bluesky_core.plans.bp.adaptive_scan") as bp_scan,
+        patch("ibex_bluesky_core.plans.ensure_connected"),
+    ):
+        RE(adaptive_scan(dae, block, start, stop, min_step, max_step, target_delta, rel=False))
+    bp_scan.assert_called_once()
+    assert dae in bp_scan.call_args[1]["detectors"]
+    assert block == bp_scan.call_args[1]["motor"]
+    assert start == bp_scan.call_args[1]["start"]
+    assert stop == bp_scan.call_args[1]["stop"]
+    assert min_step == bp_scan.call_args[1]["min_step"]
+    assert max_step == bp_scan.call_args[1]["max_step"]
+    assert target_delta == bp_scan.call_args[1]["target_delta"]
+
+
+def test_adaptive_scan_does_relative_scan_when_relative_true(RE, dae, block):
+    start = 0
+    stop = 2
+    min_step = 0.01
+    max_step = 0.1
+    target_delta = 0.5
+    with (
+        patch("ibex_bluesky_core.plans.bp.rel_adaptive_scan") as bp_scan,
+        patch("ibex_bluesky_core.plans.ensure_connected"),
+    ):
+        RE(adaptive_scan(dae, block, start, stop, min_step, max_step, target_delta, rel=True))
+    bp_scan.assert_called_once()
+    assert dae in bp_scan.call_args[1]["detectors"]
+    assert block == bp_scan.call_args[1]["motor"]
+    assert start == bp_scan.call_args[1]["start"]
+    assert stop == bp_scan.call_args[1]["stop"]
+    assert min_step == bp_scan.call_args[1]["min_step"]
+    assert max_step == bp_scan.call_args[1]["max_step"]
+    assert target_delta == bp_scan.call_args[1]["target_delta"]
+
 
 # test scan summarise
-
 # test adaptive_scan summarise
+
+# test save_run for scan
+# test save_run for adaptive_scan
+
+# test periods for scan
+# test periods for adaptive_scan
+
+# test polling plan
