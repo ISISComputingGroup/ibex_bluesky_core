dcab8257d2653e40d0eebf049e691d27d84bf269
Jack Doughty <jackb.doughty@btinternet.com>
2025-03-07 14:23:53 +0000

    Refactoring & ability to do rescanning

--- src/ibex_bluesky_core/plans/reflectometry/autoalign_utils.py
+++ src/ibex_bluesky_core/plans/reflectometry/autoalign_utils.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from itertools import chain
 from pathlib import Path
 from typing import Callable, NotRequired, TypedDict, Unpack
@@ -21,30 +21,33 @@ FILE_OUTPUT_DIR = Path("C:\\") / "instrument" / "var" / "logs" / "bluesky" / "ou
 
 @dataclass
 class AlignmentParam:
-        
     name: str
-    rel_scan_range: float
+    rel_scan_ranges: list[float]
     fit_method: FitMethod
     fit_param: str
-    initial_pos: float = 0.0
+    pre_align_param_positions: dict[str, float] = field(default_factory=dict)
     do_checks: Callable[[ModelResult, float], bool] | None = None
+    _pos: float = 0.0
     _prefix: str = get_pv_prefix()
-    _device: ReflParameter | BlockRw[float] | None = None
+    _alignment_param: ReflParameter | BlockRw[float] | None = None
 
-    def get_device(self):
-        if not self._device:
-            self._device = ReflParameter(prefix=self._prefix, name=self.name)
-        return self._device
-    
-    def get_tuple(self):
-        return (self.get_device(), self.initial_pos)
+    def get_signal(self):
+        if not self._alignment_param:
+            self._alignment_param = ReflParameter(prefix=self._prefix, name=self.name)
+        return self._alignment_param
 
-
-def pre_align(
-    axes_list: list[AlignmentParam],
-) -> Generator[Msg, None, None]:
-    
-    yield from bps.mv(*chain(*[(p.get_device(), p.initial_pos) for p in axes_list])) # type: ignore
+    def pre_align_params(
+        self,
+        alignment_params: list["AlignmentParam"],
+    ) -> Generator[Msg, None, None]:
+        yield from bps.mv(
+            *chain(
+                *[
+                    (p.get_signal(), self.pre_align_param_positions[p.name])
+                    for p in alignment_params
+                ]
+            )  # type: ignore
+        )
 
 
 def _add_fields(
@@ -52,7 +55,7 @@ def _add_fields(
     dae: SimpleDae | BlockR[float],
     periods: bool,
     save_run: bool,
-    device_name: str,
+    alignment_param_name: str,
 ) -> list[str]:
     if fields is []:
         if type(dae) is SimpleDae:
@@ -71,116 +74,162 @@ def _add_fields(
         else:
             fields.append(dae.name)
 
-        fields.append(device_name)
+        fields.append(alignment_param_name)
 
     return fields
 
 
 def _check_parameter(
-    param: float,
+    alignment_param_value: float,
     result: ModelResult,
     init_mot_pos: float,
     rel_scan_range: float,
-    user_checks: Callable[[ModelResult, float], bool] | None
+    user_checks: Callable[[ModelResult, float], bool] | None, # True means problem found.
 ) -> bool:
+    # Check that found param is within scan range. True means problem.
 
-    #Check that found param is within scan range
+    if init_mot_pos + rel_scan_range < alignment_param_value:
+        return True
 
-    if init_mot_pos + rel_scan_range < param:
+    elif init_mot_pos - rel_scan_range > alignment_param_value:
+        return True
+
+    if user_checks is None:
+        return False
+    return user_checks(result, alignment_param_value)
+
+
+def _inner_loop(
+    icc: ICC,
+    dae: SimpleDae | BlockR[float],
+    alignment_param: AlignmentParam,
+    rel_scan_range: float,
+    num_points: int,
+    callback_if_problem: Callable[[], Generator[Msg, None, None]] | Callable[[], None],
+) -> Generator[Msg, None, bool]:
+    print(f"Scanning over {alignment_param.name} with a relative scan range of {rel_scan_range}.")
+
+    init_mot_pos: float = yield from bps.rd(alignment_param.get_signal())
+
+    @icc
+    def _inner() -> Generator[Msg, None, None]:
+        yield from bp.rel_scan(
+            [dae],
+            alignment_param.get_signal(),
+            -rel_scan_range,
+            rel_scan_range,
+            num=num_points,
+        )
+
+    yield from _inner()
+
+    found_problem = True
+    if icc.live_fit.result is not None:
+        print(icc.live_fit.result.fit_report())
+        print(f"Files written to {FILE_OUTPUT_DIR}\n")
+        alignment_param_value = icc.live_fit.result.values[alignment_param.fit_param]
+
+        found_problem = _check_parameter(
+            alignment_param_value=alignment_param_value,
+            result=icc.live_fit.result,
+            init_mot_pos=init_mot_pos,
+            rel_scan_range=rel_scan_range,
+            user_checks=alignment_param.do_checks,
+        )
+
+    if found_problem:
+        if type(callback_if_problem) is Callable[[], Generator[Msg, None, None]]:
+            yield from callback_if_problem()
+        else:
+            callback_if_problem()
+
+        print(
+            f"This failed one or more of the checks on {alignment_param.name} with a scan range of {rel_scan_range}."
+        )
+        print(f"Value found for {alignment_param.fit_param} was {alignment_param_value}.")
+        input("Check your setup and checking function then press enter to go again...")
+
+        return True
+
+    else:
+        print(f"Moving {alignment_param.name} to {alignment_param_value} and rezeroing...")
+        yield from bps.mv(alignment_param.get_signal(), alignment_param_value)  # type: ignore
+        # redefine motor pos to be 0 TODODDDDDDDDDDDDDDODODODOD!!!!!!!!!!!!!!!!!!!!!!!!!
         return False
 
-    elif init_mot_pos - rel_scan_range > param:
-        return False
 
-    if user_checks is None: return True
-    return user_checks(result, param)
-        
 class OptimiseAxisParams(TypedDict):
-
     num_points: NotRequired[int]
     fields: NotRequired[list[str]]
     periods: NotRequired[bool]
     save_run: NotRequired[bool]
     files_output_dir: NotRequired[Path]
-    user_checks: NotRequired[Callable[[ModelResult, float], bool] | None]
-    callback_if_problem: NotRequired[Callable[[], None]]
+    callback_if_problem: NotRequired[Callable[[], Generator[Msg, None, None]] | Callable[[], None]]
+    callback_pre_align: NotRequired[Callable[[], Generator[Msg, None, None]] | Callable[[], None]]
+    callback_post_align: NotRequired[Callable[[], Generator[Msg, None, None]] | Callable[[], None]]
     ax: NotRequired[Axes | None]
+
 
 def optimise_axis_against_intensity(
     dae: SimpleDae | BlockR[float],
-    device: ReflParameter | BlockRw[float],
-    fit: FitMethod,
-    optimised_param: str,
-    rel_scan_range: float,
-    **kwargs: Unpack[OptimiseAxisParams]
+    alignment_param: AlignmentParam,
+    **kwargs: Unpack[OptimiseAxisParams],
 ) -> Generator[Msg, None, ModelResult | None]:
-    
-    num_points = kwargs.get('num_points', 10)
-    fields = kwargs.get('fields', [])
-    periods = kwargs.get('periods', True)
-    save_run = kwargs.get('save_run', True)
-    files_output_dir = kwargs.get('files_output_dir', FILE_OUTPUT_DIR)
-    user_checks = kwargs.get('user_checks', None)
-    callback_if_problem = kwargs.get('callback_if_problem', lambda: None)
+    num_points = kwargs.get("num_points", 10)
+    fields = kwargs.get("fields", [])
+    periods = kwargs.get("periods", True)
+    save_run = kwargs.get("save_run", True)
+    files_output_dir = kwargs.get("files_output_dir", FILE_OUTPUT_DIR)
+    callback_if_problem = kwargs.get("callback_if_problem", lambda: None)
+    callback_pre_align = kwargs.get("callback_pre_align", lambda: None)
+    callback_post_align = kwargs.get("callback_post_align", lambda: None)
     ax = kwargs.get("ax", None)
 
     fields = _add_fields(
-        fields, dae=dae, periods=periods, save_run=save_run, device_name=device.name
+        fields,
+        dae=dae,
+        periods=periods,
+        save_run=save_run,
+        alignment_param_name=alignment_param.name,
     )
-    postfix = f"{device.name}_{datetime.now().strftime('%H%M%S')}"
-    yerr = dae.reducer.intensity_stddev.name if type(dae) is SimpleDae else None # type: ignore
+    postfix = f"{alignment_param.name}_{datetime.now().strftime('%H%M%S')}"
+    yerr = dae.reducer.intensity_stddev.name if type(dae) is SimpleDae else None  # type: ignore
 
     icc = ICC(
-        x=device.name,
+        x=alignment_param.name,
         y=dae.name,
         yerr=yerr,
         measured_fields=fields,
         live_fit_logger_output_dir=files_output_dir,
         human_readable_file_output_dir=files_output_dir,
         fields_for_hr_file=fields,
-        fit=fit,
+        fit=alignment_param.fit_method,
         live_fit_logger_postfix=postfix,
         ax=ax,
     )
 
-    init_mot_pos: float = yield from bps.rd(device)
-
-    @icc
-    def _inner() -> Generator[Msg, None, None]:
-        yield from bp.rel_scan([dae], device, -rel_scan_range, rel_scan_range, num=num_points)
-
-    yield from _inner()
-
-    if icc.live_fit.result is None:
-        return None
-
-    print(icc.live_fit.result.fit_report())
-    print(f"Files written to {FILE_OUTPUT_DIR}\n")
-    param = icc.live_fit.result.values[optimised_param]
-
-    sanity_check = _check_parameter(param=param,result=icc.live_fit.result, init_mot_pos=init_mot_pos,rel_scan_range=rel_scan_range,user_checks=user_checks)
-
-    if not sanity_check:
-        callback_if_problem()
-
-        print(f"This failed one or more of the checks on {device.name}.")
-        input("Check your setup and checking function then press enter to go again...")
-
-        yield from optimise_axis_against_intensity(
-            dae=dae,
-            device=device,
-            fit=fit,
-            rel_scan_range=rel_scan_range,
-            optimised_param=optimised_param,
-            fields=fields,
-            periods=periods,
-            save_run=save_run,
-            files_output_dir=files_output_dir,
-        )
-
+    if type(callback_pre_align) is Callable[[], Generator[Msg, None, None]]:
+        yield from callback_pre_align()
     else:
-        print(f"Moving {device.name} to {param} and rezeroing...")
-        yield from bps.mv(device, param)  # type: ignore
-        # redefine motor pos to be 0 TODODDDDDDDDDDDDDDODODODOD!!!!!!!!!!!!!!!!!!!!!!!!!
-    
+        callback_pre_align()
+
+    problem_loop = True
+    while problem_loop:
+        problem_loop = False
+
+        for rel_scan_range in alignment_param.rel_scan_ranges:
+            problem_loop = yield from _inner_loop(
+                icc,
+                dae,
+                alignment_param,
+                rel_scan_range,
+                num_points,
+                callback_if_problem,
+            )
+
+    if type(callback_post_align) is Callable[[], Generator[Msg, None, None]]:
+        yield from callback_post_align()
+    else:
+        callback_post_align()
+
     return icc.live_fit.result
--- src/ibex_bluesky_core/plans/reflectometry/polref/polref_plans.py
+++ src/ibex_bluesky_core/plans/reflectometry/polref/polref_plans.py
@@ -1,12 +1,10 @@
+from datetime import datetime
 from math import isclose
 from pathlib import Path
 import winsound
-import bluesky.plan_stubs as bps
 from ophyd_async.plan_stubs import ensure_connected
 from bluesky.utils import Msg
 from collections.abc import Generator
-from ophyd_async.epics.core import epics_signal_r
-from ibex_bluesky_core.callbacks.fitting import FitMethod
 from ibex_bluesky_core.callbacks.fitting.fitting_utils import Gaussian
 from ibex_bluesky_core.devices import get_pv_prefix
 from ibex_bluesky_core.devices.simpledae import SimpleDae
@@ -17,14 +15,16 @@ from ibex_bluesky_core.devices.simpledae.controllers import (
 from ibex_bluesky_core.devices.simpledae.reducers import MonitorNormalizer
 from ibex_bluesky_core.devices.simpledae.waiters import GoodFramesWaiter, PeriodGoodFramesWaiter
 from ibex_bluesky_core.plans import set_num_periods
-from ibex_bluesky_core.plans.reflectometry import centred_pixel, refl_parameter, autoalign_utils
-from functools import partial
+from ibex_bluesky_core.plans.reflectometry import centred_pixel, autoalign_utils
 from lmfit.model import ModelResult
+import matplotlib.pyplot as plt
 
 PREFIX = get_pv_prefix()
 COUNT = 50
 FRAMES = 500
-READABLE_FILE_OUTPUT_DIR = Path("C:\\") / "instrument" / "var" / "logs" / "bluesky" / "output_files"
+READABLE_FILE_OUTPUT_DIR = (
+    Path("C:\\") / "instrument" / "var" / "logs" / "bluesky" / "output_files" / ""
+)
 DEFAULT_DET = 3
 DEFAULT_MON = 1
 PIXEL_RANGE = 0
@@ -69,46 +69,48 @@ def get_dae(
 
 #################### ^^ above to be removed
 
-def theta_checks(result: ModelResult, param_value: float) -> bool:
 
+def s1vg_checks(result: ModelResult, param_value: float) -> bool:
     rsquared_confidence = 0.9
     expected_param_value = 1.0
-    expected_param_value_tol = 0.1 
+    expected_param_value_tol = 0.1
     max_peak_factor = 5.0
 
     if result.rsquared < rsquared_confidence:
-        return False
-    
+        return True
+
     # For the parameter you're optimising, provide a value you would expect for it, assert if its not close by a provided factor
     if not isclose(expected_param_value, param_value, abs_tol=expected_param_value_tol):
-        return False
-        
+        return True
+
     # Is the peak above the background by some factor (optional because param may not be for a peak, or background may not be a parameter in the model).
     if result.values["background"] / result.model.func(param_value) <= max_peak_factor:
-        return False
-    
-    return True
+        return True
+
+    return False
 
 
 parameters = [
-    autoalign_utils.AlignmentParam("theta", 5, Gaussian.fit(), "x0", 0.0, do_checks=theta_checks),
-    # add when on beamline?
+    autoalign_utils.AlignmentParam(
+        name="S1VG",
+        rel_scan_ranges=[0.3],
+        fit_method=Gaussian.fit(),
+        fit_param="x0",
+        do_checks=s1vg_checks,
+        pre_align_param_positions={
+            "THETA": 0,
+            "PHI": 0,
+            "S1VG": -0.1,
+            "S2VG": 10,
+            "S3N": 5,
+            "S3S": -5,
+            "S1HG": 40,
+            "S2HG": 30,
+            "S3E": 15,
+            "S3W": 15,
+        },
+    ),
 ]
-
-
-def pre_align_plan() -> Generator[Msg, None, None]:
-    """Move all given axes to their default positions at the same time."""
-
-    mode = epics_signal_r(str, f"{PREFIX}REFL_01:CONST:IS_HORIZONTAL", name="refl_mode")
-    if (yield from bps.read(mode)) != "YES":
-        return
-
-    axes_sig = [parameters[i].get_device() for i in range(len(parameters))]
-    yield from ensure_connected(*axes_sig)
-
-    print("Moving the following axes to their initial positions...")
-    print(*axes_sig)
-    yield from autoalign_utils.pre_align(parameters)
 
 
 def full_autoalign_plan() -> Generator[Msg, None, None]:
@@ -123,28 +125,27 @@ def full_autoalign_plan() -> Generator[Msg, None, None]:
 
     yield from set_num_periods(dae, COUNT if PERIODS else 1)
 
-    axes_sig = [parameters[i].get_device() for i in range(len(parameters))]
+    axes_sig = [parameters[i].get_signal() for i in range(len(parameters))]
     yield from ensure_connected(*axes_sig, dae)
 
     print("Starting auto-alignment...")
 
     for i in range(len(parameters)):
-        print(f"Next axis is {parameters[i].name}")
+        print(f"Next alignment parameter is {parameters[i].name}")
         winsound.Beep(2500, 200)
 
         yield from autoalign_utils.optimise_axis_against_intensity(
             dae=dae,
-            device=parameters[i].get_device(),
-            fit=parameters[i].fit_method,
-            optimised_param=parameters[i].fit_param,
-            rel_scan_range=parameters[i].rel_scan_range,
+            alignment_param=parameters[i],
             fields=[],
             periods=PERIODS,
             save_run=SAVE_RUN,
             files_output_dir=READABLE_FILE_OUTPUT_DIR,
-            callback_if_problem=partial(winsound.Beep, 1000, 500),
+            callback_if_problem=lambda: winsound.Beep(1000, 500),
+            callback_pre_align=lambda: parameters[i].pre_align_params(parameters),
+            callback_post_align=lambda: plt.savefig(
+                fname=f"{READABLE_FILE_OUTPUT_DIR}{parameters[i].name}_{datetime.now().strftime('%H%M%S')}"
+            ),
         )
 
-
-def sample_autoalign_plan():
-    pass
+    print("Auto alignment complete.")
