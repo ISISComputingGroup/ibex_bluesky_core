10fd38ae8a17ac6e997d35dd74d35429f0ce4e30
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-03-11 18:16:02 +0000

    path

--- tests/callbacks/test_utils.py
+++ tests/callbacks/test_utils.py
@@ -1,5 +1,8 @@
+import os
 import sys
 from unittest.mock import patch
+
+import pytest
 
 
 def test_default_output_location_with_env_var():
@@ -10,9 +13,10 @@ def test_default_output_location_with_env_var():
         assert str(DEFAULT_PATH) == "foo"
 
 
+@pytest.mark.skipif(os.name != "nt", reason="Windows only")
 def test_default_output_location_without_env_var():
     with patch("ibex_bluesky_core.callbacks._utils.os.environ.get", return_value=None):
         del sys.modules["ibex_bluesky_core.callbacks._utils"]
         from ibex_bluesky_core.callbacks._utils import DEFAULT_PATH  # noqa PLC0415
 
-        assert "isis.cclrc.ac.uk" in str(DEFAULT_PATH)
+        assert str(DEFAULT_PATH).startswith(r"\\isis.cclrc.ac.uk")
