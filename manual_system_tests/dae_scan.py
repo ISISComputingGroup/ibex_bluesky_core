be6545009e27f77bc8ddf69fdd0b5779f635627e
Jack Harper <harps912@gmail.com>
2025-07-01 14:05:44 +0100

    remove comment about bluesky not being typed yet

--- manual_system_tests/dae_scan.py
+++ manual_system_tests/dae_scan.py
@@ -93,7 +93,6 @@ def dae_scan_plan() -> Generator[Msg, None, None]:
 
     @icc
     def _inner() -> Generator[Msg, None, None]:
-        # Pyright does not understand as bluesky isn't typed yet
         yield from bp.scan([dae], block, 0, 10, num=NUM_POINTS)
         print(icc.live_fit.result.fit_report())
         print(f"COM: {icc.peak_stats['com']}")
