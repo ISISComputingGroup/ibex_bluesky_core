1e5ad60805e22c61a6ccacab3442d9a78b278969
AWaseem18 <aawaseem18@gmail.com>
2025-07-02 14:01:45 +0100

    ruff and pyright

--- tests/callbacks/fitting/test_fitting_methods.py
+++ tests/callbacks/fitting/test_fitting_methods.py
@@ -17,15 +17,13 @@ from ibex_bluesky_core.fitting import (
     Gaussian,
     Linear,
     Lorentzian,
+    MuonMomentum,
     NegativeTrapezoid,
     Polynomial,
     SlitScan,
     TopHat,
     Trapezoid,
-    MuonMomentum,
 )
-
-
 
 
 class MockFit(Fit):
@@ -838,13 +836,16 @@ def values() -> dict[str, float]:
         "p": 1.82,
     }
     return value
+
+
 @pytest.fixture
 def x() -> npt.NDArray[np.float64]:
     xaxis = np.linspace(20.5, 23, 15, dtype=np.float64)
     return xaxis
 
+
 @pytest.fixture
-def out(x: npt.NDArray[np.float64],values: dict[str,float]) -> npt.NDArray[np.float64]:
+def out(x: npt.NDArray[np.float64], values: dict[str, float]) -> npt.NDArray[np.float64]:
     out1 = MuonMomentum.model().func(
         x=x,
         x0=values["x0"],
@@ -858,57 +859,62 @@ def out(x: npt.NDArray[np.float64],values: dict[str,float]) -> npt.NDArray[np.fl
 
 class TestMuonMomentum:
     class TestMuonMomentumModel:
-
-        def test_muon_momentum_model_w(self,out: npt.NDArray[np.float64],values: dict[str, float],x: npt.NDArray[np.float64]):
-
-            out2=MuonMomentum.model().func(
-            x=x,
-            x0=values["x0"],
-            w=values["w"]+1,
-            r=values["r"],
-            b=values["b"],
-            p=values["p"],
+        def test_muon_momentum_model_w(
+            self, out: npt.NDArray[np.float64], values: dict[str, float], x: npt.NDArray[np.float64]
+        ):
+            out2 = MuonMomentum.model().func(
+                x=x,
+                x0=values["x0"],
+                w=values["w"] + 1,
+                r=values["r"],
+                b=values["b"],
+                p=values["p"],
             )
 
             assert np.mean(out) < np.mean(out2)
 
-        def test_muon_momentum_model_b(self,out: npt.NDArray[np.float64],values: dict[str, float],x: npt.NDArray[np.float64]):
-
+        def test_muon_momentum_model_b(
+            self, out: npt.NDArray[np.float64], values: dict[str, float], x: npt.NDArray[np.float64]
+        ):
             out2 = MuonMomentum.model().func(
                 x=x,
                 x0=values["x0"],
                 w=values["w"],
                 r=values["r"],
-                b=values["b"]+1,
+                b=values["b"] + 1,
                 p=values["p"],
             )
 
             assert np.min(out) < np.min(out2)
 
-        def test_muon_momentum_model_r(self,out: npt.NDArray[np.float64],values: dict[str, float],x: npt.NDArray[np.float64]):
+        def test_muon_momentum_model_r(
+            self, out: npt.NDArray[np.float64], values: dict[str, float], x: npt.NDArray[np.float64]
+        ):
             out2 = MuonMomentum.model().func(
                 x=x,
                 x0=values["x0"],
                 w=values["w"],
-                r=values["r"]+10,
+                r=values["r"] + 10,
                 b=values["b"],
                 p=values["p"],
             )
 
             assert np.max(out) - np.min(out) < np.max(out2) - np.min(out2)
 
-        def test_muon_momentum_model_x0(self, out: npt.NDArray[np.float64], values: dict[str, float], x: npt.NDArray[np.float64]):
+        def test_muon_momentum_model_x0(
+            self, out: npt.NDArray[np.float64], values: dict[str, float], x: npt.NDArray[np.float64]
+        ):
             out2 = MuonMomentum.model().func(
                 x=x,
-                x0=values["x0"]+1,
+                x0=values["x0"] + 1,
                 w=values["w"],
                 r=values["r"],
                 b=values["b"],
                 p=values["p"],
             )
 
-            assert x[np.argmax(out)]< x[np.argmax(out2)]
-            assert x[np.argmin(out)]< x[np.argmin(out2)]
+            assert x[np.argmax(out)] < x[np.argmax(out2)]
+            assert x[np.argmin(out)] < x[np.argmin(out2)]
 
     class TestMuonMomentumGuess:
         pass
