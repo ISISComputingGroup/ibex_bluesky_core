ff06540c08312bf13b70ab03e507f98ef2548c88
Jack Doughty <jackb.doughty@btinternet.com>
2025-07-02 13:13:23 +0100

    docs change

--- src/ibex_bluesky_core/devices/polarisingdae/_spectra.py
+++ src/ibex_bluesky_core/devices/polarisingdae/_spectra.py
@@ -51,9 +51,9 @@ class _PolarisedWavelengthBand(StandardReadable):
     """Subdevice that holds polarisation info for two wavelength bands.
 
     Represents the polarisation information calculated using measurements
-    taken from two `WavelengthBand` objects, one published from an "up state"
-    `WavelengthBoundedNormalizer`and the other from a "down state"
-    `WavelengthBoundedNormalizer`. Has a setter method to assign values to
+    taken from two :obj:`ibex_bluesky_core.devices.polarisingdae._spectra._WavelengthBand` objects, one published from an "up state"
+    :obj:`ibex_bluesky_core.devices.polarisingdae.MultiWavelengthBandNormalizer`and the other from a "down state"
+    :obj:`ibex_bluesky_core.devices.polarisingdae.MultiWavelengthBandNormalizer. Has a setter method to assign values to
     the published signals.
     """
 
