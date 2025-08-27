a1a43ed0b0dc188e36a477efa387aec7cc539643
Jack Harper <harps912@gmail.com>
2025-08-27 16:54:14 +0100

    remove python ver conditional import

--- tests/devices/test_block.py
+++ tests/devices/test_block.py
@@ -24,12 +24,6 @@ from ibex_bluesky_core.devices.block import (
 )
 from tests.conftest import MOCK_PREFIX
 
-if sys.version_info < (3, 11):
-    aio_timeout_error = asyncio.exceptions.TimeoutError
-else:
-    aio_timeout_error = TimeoutError
-
-
 async def _make_block(clazz):
     block = clazz(float, MOCK_PREFIX, "float_block")
     await block.connect(mock=True)
@@ -191,7 +185,7 @@ async def test_block_set_with_timeout():
 
     set_mock_value(block.readback, 10)
 
-    with pytest.raises(aio_timeout_error):
+    with pytest.raises(TimeoutError):
         await block.set(20)
 
     func.assert_called_once_with(20, 10)
@@ -233,7 +227,7 @@ async def test_block_set_waiting_for_global_moving_flag_timeout():
 
     set_mock_value(block.global_moving, True)
     with patch("ibex_bluesky_core.devices.block.asyncio.sleep") as mock_aio_sleep:
-        with pytest.raises(aio_timeout_error):
+        with pytest.raises(TimeoutError):
             await block.set(10)
         # Only check first call, as wait_for_value from ophyd_async gives us a few more...
         assert mock_aio_sleep.mock_calls[0] == call(GLOBAL_MOVING_FLAG_PRE_WAIT)
@@ -360,13 +354,13 @@ async def test_block_mot_set(mot_block):
 async def test_block_failing_write(timeout_is_error):
     block = await _block_with_write_config(BlockWriteConfig(timeout_is_error=timeout_is_error))
 
-    get_mock_put(block.setpoint).side_effect = aio_timeout_error
+    get_mock_put(block.setpoint).side_effect = TimeoutError
 
-    with pytest.raises(aio_timeout_error) if timeout_is_error else nullcontext():
+    with pytest.raises(TimeoutError) if timeout_is_error else nullcontext():
         await block.set(1)
 
 
 async def test_block_failing_write_with_default_write_config(writable_block):
-    get_mock_put(writable_block.setpoint).side_effect = aio_timeout_error
-    with pytest.raises(aio_timeout_error):
+    get_mock_put(writable_block.setpoint).side_effect = TimeoutError
+    with pytest.raises(TimeoutError):
         await writable_block.set(1)
